d04b6d81375f1b584ff0ed6b96cfa126f41a129e
Ho-Wan <ho.wan2345@gmail.com>
2020-03-01 18:22:09 +0000

    wip excl China, add tooltips

--- src/chart/ChartCard.tsx
+++ src/chart/ChartCard.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, TooltipProps, XAxis, YAxis } from "recharts";
 import styled from "styled-components";
-import { DateData, FlatData, FlatCountryCase } from "./chart.types";
+import { DateData, FlatCountryCase, FlatData } from "./chart.types";
 import { fetchDataAction } from "./redux/chart.actions";
 import { chartSelectors } from "./redux/chart.reducer";
-import { CHART_PROPS, COLORS } from "./utils/constants";
+import { CHART_COLORS, CHART_PROPS, COLORS } from "./utils/constants";
 
 const StyledChartCardDiv = styled.div`
   /* auto margin 800w - responsive to fit screen width */
@@ -24,7 +24,8 @@ const StyledChartCardDiv = styled.div`
   border: 1px solid ${COLORS.mediumGrey};
 `;
 
-const flattenData = function(data: DateData[]): FlatData[] {
+const flattenData = function(data: DateData[]) {
+  const dataKeySet: Set<string> = new Set();
   const dataFlattened = data.map((d, i) => {
     // sum up cases by country and store in temp dict
     const dict: FlatCountryCase = {};
@@ -34,6 +35,8 @@ const flattenData = function(data: DateData[]): FlatData[] {
       } else {
         dict[rd.country] += rd.cases;
       }
+
+      dataKeySet.add(rd.country);
     });
 
     return {
@@ -41,9 +44,27 @@ const flattenData = function(data: DateData[]): FlatData[] {
       date: d.date,
     } as FlatData;
   });
+  const dataKeys = Array.from(dataKeySet);
 
-  return dataFlattened;
+  return { dataFlattened, dataKeys };
 };
+
+// function CustomTooltip(e: any) {
+//   if (e == null || e.props == null) return null;
+//   const { active } = e.props;
+
+//   if (active) {
+//     const { payload, label } = e.props;
+//     return (
+//       <div className="custom-tooltip">
+//         <p className="label">{`${label} : ${payload && payload[0].value}`}</p>
+//         <p className="desc">Anything you want can be displayed here.</p>
+//       </div>
+//     );
+//   }
+
+//   return null;
+// }
 
 function ChartCard() {
   const dispatch = useDispatch();
@@ -54,7 +75,21 @@ function ChartCard() {
   }, []);
 
   const data = useSelector(chartSelectors.dataSelector);
-  const dataFlattened = flattenData(data);
+  const { dataFlattened, dataKeys } = flattenData(data);
+
+  const dataKeysFiltered = dataKeys.filter(v => v !== "China");
+
+  function customTooltip(e: TooltipProps) {
+    if (e.active && e.payload != null && e.payload[0] != null) {
+      return (
+        <div className="custom-tooltip">
+          <p>{e.payload[0].payload["dataKey"]}</p>
+        </div>
+      );
+    } else {
+      return "";
+    }
+  }
 
   return (
     <>
@@ -65,11 +100,18 @@ function ChartCard() {
             <LineChart data={dataFlattened} margin={{ top: 5, right: 20, left: 0, bottom: 5 }}>
               <XAxis dataKey="date" />
               <YAxis />
-              <Tooltip />
+              <Tooltip content={customTooltip} />
               <CartesianGrid strokeDasharray="3 3" />
-              <Line type="monotone" dataKey="US" stroke="#ff7300" yAxisId={0} animationDuration={500} />
-              <Line type="monotone" dataKey="UK" stroke="#ff7300" yAxisId={0} animationDuration={500} />
-              <Line type="monotone" dataKey="South Korea" stroke="#ff7300" yAxisId={0} animationDuration={500} />
+              {dataKeysFiltered.map((dKey, i) => (
+                <Line
+                  key={i}
+                  type="monotone"
+                  dataKey={dKey}
+                  stroke={CHART_COLORS[i]}
+                  yAxisId={0}
+                  animationDuration={500}
+                />
+              ))}
             </LineChart>
           </ResponsiveContainer>
         )}
--- src/chart/utils/constants.tsx
+++ src/chart/utils/constants.tsx
@@ -1,19 +1,54 @@
 export const COLORS = {
-  mediumGrey: '#9e9e9e',
-  white: '#ffffff',
-  primary: '#7433ff',
-  secondary: '#50e3c2',
+  mediumGrey: "#9e9e9e",
+  white: "#ffffff",
+  primary: "#7433ff",
+  secondary: "#50e3c2",
 };
 
 export const COVID_API_URL = {
-  baseUrl: 'https://covid-api.spandraw.com',
-  data: 'data',
+  baseUrl: "https://covid-api.spandraw.com",
+  data: "data",
 };
 
 export const CHART_PROPS = {
   chartWidth: 900,
-}
+};
 
 export const CHART_ID = {
-  chart: 'chart',
-}
+  chart: "chart",
+};
+
+export const CHART_COLORS = [
+  "#3366cc",
+  "#dc3912",
+  "#ff9900",
+  "#109618",
+  "#990099",
+  "#0099c6",
+  "#dd4477",
+  "#66aa00",
+  "#b82e2e",
+  "#316395",
+  "#3366cc",
+  "#994499",
+  "#22aa99",
+  "#aaaa11",
+  "#6633cc",
+  "#e67300",
+  "#8b0707",
+  "#651067",
+  "#329262",
+  "#5574a6",
+  "#3b3eac",
+  "#b77322",
+  "#16d620",
+  "#b91383",
+  "#f4359e",
+  "#9c5935",
+  "#a9c413",
+  "#2a778d",
+  "#668d1c",
+  "#bea413",
+  "#0c5922",
+  "#743411",
+];
